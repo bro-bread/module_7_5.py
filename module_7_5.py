86aa4a2d4ffb2134d95598c992ce59530609a01b
bro-bread <sasaglebov928@gmail.com>
2024-11-20 02:34:44 +0700

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,11 +1,28 @@
 import os
+import time
 
-directory = os.path.join(".")
-for i in os.walk(directory):
-    print(i)
-print(" ")
-print(os.path.getmtime(directory))
-print(" ")
-print(os.path.getsize(directory))
-print(" ")
-print(os.path.dirname(directory))
+# Дирикторий
+directory = "."
+
+
+def explore_directory():
+    # Обход каталогов
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # Формируем путь к файлу
+            filepath = os.path.join(root, file)
+
+            # Получаем время изм файла
+            filetime = os.path.getmtime(filepath)
+            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+            # Размер файла
+            filesize = os.path.getsize(filepath)
+
+            # родительскую директория
+            parent_dir = os.path.dirname(filepath)
+
+            # инфа о файле
+            print(
+                f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
